2e3a0ad24a584c6dfe9e22163d6ada6293f39655
Jeff Mendez <jeffmendez19@gmail.com>
2020-10-14 13:10:39 -0400

    chore: update env examples

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,29 +2,33 @@
 
 set -e
 
+echo "Project being boostraped"
+
+cp .env.example.txt .env
+
 # Install web
 cd web
-(npm install && npm run build) &
+(npm install && npm run build) & cp .env.example.txt .env &
 cd ..
 
 # Install api
 cd api
-(npm install && npm run build) &
+(npm install && npm run build) & cp .env.example.txt .env &
 cd ..
 
 # Install angelica
 cd angelica
-(npm install && npm run build) &
+(npm install && npm run build) & cp .env.example.txt .env &
 cd ..
 
 # Install cdn-server
 cd cdn-server
-(npm install && npm run build) &
+(npm install && npm run build) & cp .env.example.txt .env &
 cd ..
 
 # Install mav
 cd mav
-(npm install && npm run build) &
+(npm install && npm run build) & cp .env.example.txt .env &
 cd ..
 
 # Install ui
@@ -34,23 +38,23 @@ cd ..
 
 # Install example-site
 cd example-site
-(brew install zola) &
+(brew install zola && brew upgrade zola) &
 cd ..
 
 # Install iframe-server
 cd iframe-server
-(npm install && npm run build) &
+(npm install && npm run build) & cp .env.example.txt .env &
 cd ..
 
 # Install logger
 cd logger
-(npm install && npm run build) &
+(npm install && npm run build) & cp .env.example.txt .env &&
 cd ..
 
 # Install watcher and make sure RUST is installed. For windows make sure to go to https://www.rust-lang.org/tools/install and manually cd into the folder and run cargo run
 cd watcher
-(curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh && cargo run) &
+(curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh && cargo run) & cp .env.example.txt .env &
 cd ..
 
 
-echo "Project bootstraped and ready to run in docker or local environments"
+echo "Ready to run in docker or local environments"
---
+++ environment-variables.md
@@ -0,0 +1,19 @@
+# Environment Variables
+
+- [Use `.env` to load environment variables](#loading-environment-variables)
+
+## Loading Environment Variables
+
+A11yWatch has support for loading environment variables from `.env` files.
+
+An example `.env`:
+
+```bash
+DB_HOST=localhost
+DB_USER=myuser
+DB_PASS=mypassword
+```
+
+You can setup the .env file by running the `chmod +x ./bootstrap.sh && ./bootstrap.sh` script in the root of the project
+
+> **Note**: we only use one type of .env which is the main .env file for detection. Partains 12factor rules `.gitignore`.
