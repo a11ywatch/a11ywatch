f6a066b297be0dd682ea972490a6e0b918996f74
Jeff Mendez <jeffmendez19@gmail.com>
2020-10-14 18:59:52 -0400

    fix: bootstrap setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,57 +4,59 @@ set -e
 
 echo "Project being boostraped"
 
-cp .env.example.txt .env
+cpenv="cp .env.example.txt .env"
+node_build="npm install && npm run build"
+
+($cpenv) &
 
 # Install web
 cd web
-(npm install && npm run build) & cp .env.example.txt .env &
+($cpenv && $node_build) &
 cd ..
 
 # Install api
 cd api
-(npm install && npm run build) & cp .env.example.txt .env &
+($cpenv && $node_build) &
 cd ..
 
 # Install angelica
 cd angelica
-(npm install && npm run build) & cp .env.example.txt .env &
+($cpenv && $node_build) &
 cd ..
 
 # Install cdn-server
 cd cdn-server
-(npm install && npm run build) & cp .env.example.txt .env &
+($cpenv && $node_build) &
 cd ..
 
 # Install mav
 cd mav
-(npm install && npm run build) & cp .env.example.txt .env &
+($cpenv && $node_build) &
 cd ..
 
 # Install ui
 cd ui
-(npm install && npm run build) &
+($node_build) &
 cd ..
 
 # Install example-site
 cd example-site
-(brew install zola && brew upgrade zola) &
+($cpenv && brew install zola && brew upgrade zola) &
 cd ..
 
 # Install iframe-server
 cd iframe-server
-(npm install && npm run build) & cp .env.example.txt .env &
+($cpenv && $node_build) &
 cd ..
 
 # Install logger
 cd logger
-(npm install && npm run build) & cp .env.example.txt .env &&
+($cpenv && $node_build) &
 cd ..
 
 # Install watcher and make sure RUST is installed. For windows make sure to go to https://www.rust-lang.org/tools/install and manually cd into the folder and run cargo run
 cd watcher
-(curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh && cargo run) & cp .env.example.txt .env &
+($cpenv && curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh && cargo run) &
 cd ..
-
 
 echo "Ready to run in docker or local environments"
