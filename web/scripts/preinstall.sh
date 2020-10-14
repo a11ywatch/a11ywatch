0eb29c0c6a25af759acd7311f8abe4f88a800625
Jeff Mendez <jeffmendez19@gmail.com>
2020-10-13 23:38:37 -0400

    chore(web): cleanup unused gh package setups

--- web/scripts/preinstall.sh
+++
@@ -1,8 +0,0 @@
-    #!/bin/bash
-
-	if [[ -n "$GITHUB_TOKEN" ]]; then
-		echo "@a11ywatch:registry=https://npm.pkg.github.com" >> .npmrc
-		echo "//npm.pkg.github.com/:_authToken=$GITHUB_TOKEN" >> .npmrc
-	else
-		echo "missing github token"
-	fi
