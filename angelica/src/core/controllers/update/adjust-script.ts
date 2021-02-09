5e59a9f2423af4cd050b1b03e085e98026ded8ef
Jeff Mendez <jeffmendez19@gmail.com>
2021-02-08 22:00:44 -0500

    chore(angelica): remove space at script adjust for codecov test

--- angelica/src/core/controllers/update/adjust-script.ts
+++ angelica/src/core/controllers/update/adjust-script.ts
@@ -3,7 +3,6 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  **/
-
 import { fork } from "child_process";
 import { format } from "prettier";
 import { skipNavigationMethod, scriptBuild } from "@app/core/lib";
