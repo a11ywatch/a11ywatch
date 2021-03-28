ebc5bd7d949fb33ba528e199f2b7ea1b08a3550d
Jeff Mendez <jeffmendez19@gmail.com>
2021-03-28 15:20:33 -0400

    chore(email): improve email template sending

--- api/src/core/email-templates/issues-found.ts
+++ api/src/core/email-templates/issues-found.ts
@@ -22,17 +22,17 @@ const issuesFoundTemplate: IssuesFound = (
 
   if (data.issues?.length) {
     data.issues.some((item: Issue, i: number) => {
-      if (i === 4) {
-        return;
+      if (i >= 4) {
+        return true;
       }
-      listData = `${listData}<li><div>${item?.context}</div><div style="font-weight: 400; font-size: 1em;">${item?.message}</div></li>`;
+      listData = `${listData}<li><div style="font-weight: 500;">${item?.context}</div><div style="font-weight: 400; font-size: 1em;">${item?.message}</div></li>`;
       return false;
     });
   }
 
-  return `<h1>${data?.issues?.length} issues found for ${data?.pageUrl}!</h1>
-  <ul>${listData}</ul>
-  <a href="https://www.a11ywatch.com/dashboard" style="font-weight: 800; font-size: 1.2em; display: block; background: #5c6bc0; padding: 10px; border-radius: 4px; color: white; text-align: center; text-decoration: none;">View Details</a>
+  return `<h1>${data?.issues?.length} issues found for ${data?.pageUrl}</h1>
+  <ul style="list-style-type: none;">${listData}</ul>
+  <a href="https://www.a11ywatch.com/dashboard" style="font-weight: 800; font-size: 2em; display: block; background: #5c6bc0; padding: 10px; color: white; text-align: center; text-decoration: none;">View Details</a>
   <p style="margin-top:10; margin-bottom: 10px;">If you want to stop receiving emails toggle the alert setting to off on the dashboard</p>
 `;
 };
