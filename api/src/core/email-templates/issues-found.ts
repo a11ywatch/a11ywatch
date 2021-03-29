ba2d8f353bfb0ae31d5969a610e14e09c01ad021
Jeff Mendez <jeffmendez19@gmail.com>
2021-03-29 07:44:46 -0400

    chore(api): add download report button on email issue template

--- api/src/core/email-templates/issues-found.ts
+++ api/src/core/email-templates/issues-found.ts
@@ -22,17 +22,18 @@ const issuesFoundTemplate: IssuesFound = (
 
   if (data.issues?.length) {
     data.issues.some((item: Issue, i: number) => {
-      if (i >= 4) {
+      if (i === 4) {
         return true;
       }
-      listData = `${listData}<li><div style="font-weight: 500;">${item?.context}</div><div style="font-weight: 400; font-size: 1em;">${item?.message}</div></li>`;
+      listData = `${listData}<li><div><div style="font-weight: 800; font-size: 15px;">${item?.context}</div><div style="font-weight: 400; font-size: 1em;">${item?.message}</div></div></li>`;
       return false;
     });
   }
-
-  return `<h1>${data?.issues?.length} issues found for ${data?.pageUrl}</h1>
+  const page = data?.pageUrl;
+  return `<h1>${data?.issues?.length} issues found for ${page}</h1>
   <ul style="list-style-type: none;">${listData}</ul>
-  <a href="https://www.a11ywatch.com/dashboard" style="font-weight: 800; font-size: 2em; display: block; background: #5c6bc0; padding: 10px; color: white; text-align: center; text-decoration: none;">View Details</a>
+  <a href="https://www.a11ywatch.com/dashboard" style="font-weight: 800; font-size: 1.8em; display: block; background: #5c6bc0; padding: 8px; color: white; text-align: center; text-decoration: none; margin-bottom: 10px;">View Details</a>
+  <a href="https://www.api.a11ywatch.com/api/get-website?q=${page}&download=true" style="font-weight: 800; font-size: 1.8em; display: block; background: #fff; padding: 8px; color: #ccc; text-align: center; text-decoration: none;">Download Report</a>
   <p style="margin-top:10; margin-bottom: 10px;">If you want to stop receiving emails toggle the alert setting to off on the dashboard</p>
 `;
 };
