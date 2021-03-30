6bb73222289baa4e68498d1827bb726abde824d2
Jeff Mendez <jeffmendez19@gmail.com>
2021-03-29 22:21:37 -0400

    chore(logger): move logger to another repo https://github.com/A11yWatch/logger

--- logger/src/lib/config.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) A11yWatch, LLC. and its affiliates.
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- **/
-
-export const DEV: boolean = process.env.NODE_ENV !== "production";
-export const PORT: number =
-  process.env.NODE_ENV === "test" ? 0 : parseInt(process.env.PORT, 10) || 8022;
