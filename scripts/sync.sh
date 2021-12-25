a62a31cb62c79ae8fc559cb191cd046d784999d8
j-mendez <jeff@a11ywatch.com>
2021-12-25 06:36:17 -0500

    chore(sync): add elastic cdn to sync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -7,3 +7,4 @@ cd ../crawler && git pull origin main
 cd ../pagemind && git pull origin main
 cd ../mav && git pull origin main
 cd ../logger && git pull origin main
+cd ../elastic-cdn && git pull origin main
