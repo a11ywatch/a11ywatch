2504669554cfe35252b7864ae3950e551bbe3e3c
j-mendez <jeff@a11ywatch.com>
2022-02-17 20:59:21 -0500

    chore(deploy): minor update script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,10 @@
 
 ./scripts/build.sh
 
-docker push a11ywatch/a11ywatch-core
-docker push a11ywatch/pagemind
-docker push a11ywatch/mav
-docker push a11ywatch/crawler
+docker push a11ywatch/a11ywatch-core &
+docker push a11ywatch/pagemind &
+wait
+docker push a11ywatch/mav &
+docker push a11ywatch/crawler &
+wait
 docker push a11ywatch/elastic-cdn
