288bdf76ed31f7460a974bd76ed0e56b780e9a01
j-mendez <jeff@a11ywatch.com>
2022-03-06 02:01:29 -0500

    chore(cli): add tmp folder contents

--- cli/src/builders/temp.rs
+++ cli/src/builders/temp.rs
@@ -1,16 +1,19 @@
 use crate::generators::compose;
-use std::fs::File;
+use std::fs::{File, create_dir};
 use std::io::prelude::*;
 use std::path::Path;
 
 // return true if created a new compose file
 pub fn create_compose_file() -> std::io::Result<()> {
-
-    if !Path::new("a11ywatch.yml").exists() {
-        println!("creating a11ywatch.yml compose file");
-        let mut file = File::create("a11ywatch.yml")?;
+    if !Path::new("/tmp").exists() {
+        create_dir("/tmp")?;
+    }
+    if !Path::new("/tmp/a11ywatch").exists() {
+        create_dir("/tmp/a11ywatch")?;
+    }
+    if !Path::new("/tmp/a11ywatch/compose.yml").exists() {
+        let mut file = File::create("/tmp/a11ywatch/compose.yml")?;
         file.write_all(&compose::generate_compose().as_bytes())?;
     }
-
     Ok(())
 }
--- cli/src/runtime/docker.rs
+++ cli/src/runtime/docker.rs
@@ -1,26 +1,21 @@
 use std::process::Command;
 
-// TODO: DETECT IF COMPOSE NEEDS TO BE GENERATED
+// TODO: TARGET COMPOSE LOCATION OF FILE
 pub fn build_backend() {
-    println!("build: creating the application via docker...");
     Command::new("docker-compose")
-    .args(["build"])
-    .status()
-    .expect("Failed to execute command");
+        .args(["build"])
+        .status()
+        .expect("Failed to execute command");
 }
 
 pub fn start_backend() {
-    println!("up: running the application via docker...");
-
     Command::new("docker-compose")
-        .args(["-f", "a11ywatch.yml", "up", "-d"])
+        .args(["-f", "/tmp/a11ywatch/compose.yml", "up", "-d"])
         .status()
         .expect("Failed to execute command");
 }
 
 pub fn run_backend(options_run: &String) {
-    println!("run: running command to docker container...");
-
     Command::new("docker-compose")
         .args(["up", "-d", &options_run])
         .status()
