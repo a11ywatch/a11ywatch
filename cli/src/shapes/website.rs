03b6e96fa98a4ff50b5e0e84e831d6a6906161cb
j-mendez <jeff@a11ywatch.com>
2022-03-07 20:01:57 -0500

    chore(cli): cleanup issue types

--- cli/src/shapes/website.rs
+++ cli/src/shapes/website.rs
@@ -2,8 +2,11 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Issue {
+    #[serde(rename = "type")]
+    pub issue_type: String,
+    #[serde(rename = "typeCode")]
+    pub type_code: u32,
     pub code: String,
-    pub typeCode: u32,
     pub context: String,
     pub message: String,
     pub selector: String,
@@ -11,21 +14,30 @@ pub struct Issue {
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct IssuesInfo {
-    pub totalIssues: u32,
-    pub possibleIssuesFixedByCdn: u32,
-    pub issuesFixedByCdn: u32,
-    pub errorCount: u32,
-    pub warningCount: u32,
-    pub limitedCount: u32,
+    #[serde(rename = "totalIssues")]
+    pub total_issues: u32,
+    #[serde(rename = "possibleIssuesFixedByCdn")]
+    pub possible_issues_fixed_by_cdn: u32,
+    #[serde(rename = "issuesFixedByCdn")]
+    pub issues_fixed_by_cdn: u32,
+    #[serde(rename = "errorCount")]
+    pub error_count: u32,
+    #[serde(rename = "warningCount")]
+    pub warning_count: u32,
+    #[serde(rename = "limitedCount")]
+    pub limited_count: u32,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Website {
     pub url: String,
     pub domain: String,
-    pub issuesInfo: IssuesInfo,
-    pub adaScore: u32,
+    #[serde(rename = "issuesInfo")]
+    pub issues_info: IssuesInfo,
+    #[serde(rename = "adaScore")]
+    pub ada_score: u32,
     pub issue: Vec<Issue>,
     pub screenshot: String,
-    pub screenshotStill: String
+    #[serde(rename = "screenshotStill")]
+    pub screenshot_still: String
 }
