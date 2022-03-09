00d736d039bda573e0f9cabddedf62a27c1fbeba
j-mendez <jeff@a11ywatch.com>
2022-03-08 19:14:15 -0500

    chore(container): remove logger from compose

---
+++ src/scripts/greet.sh
@@ -0,0 +1,10 @@
+Greet() {
+    echo Hello "${PARAM_TO}"
+}
+
+# Will not run if sourced for bats-core tests.
+# View src/tests for more information.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    Greet
+fi
