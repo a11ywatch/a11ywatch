17c0d949305b6bc68f17e0b14315b84c8240de8a
j-mendez <jeff@a11ywatch.com>
2022-03-11 10:13:25 -0500

    chore(cli): add github env extracting

---
+++ cli/src/utils/github.rs
@@ -0,0 +1,47 @@
+use std::env;
+
+pub fn get_api() -> String {
+    // determine which ci by default
+    let mut github_repo_name: String = String::from("");
+    let mut circleci_repo_name: String = String::from("");
+
+    let mut project_user_name: String = String::from("");
+    let mut project_branch_name: String = String::from("");
+
+    for (n,v) in env::vars() {
+        if n == "CIRCLE_PROJECT_USERNAME" || n == "GITHUB_REPOSITORY_OWNER" {
+            project_user_name = v.to_string();
+        }
+        if n == "CIRCLE_BRANCH" {
+            project_branch_name = v.to_string();
+        }
+        if n == "CIRCLE_PROJECT_REPONAME" {
+            circleci_repo_name = v.to_string();
+        }
+        if n == "GITHUB_REF_NAME" {
+            project_branch_name = v.to_string().replace("refs/heads/", "");
+        }
+        if n == "GITHUB_REPOSITORY" {
+            github_repo_name = v.to_string();
+        }
+    }
+    
+    let project_repo_name: String;
+
+    // use github has exact location of repo
+    if !github_repo_name.is_empty() {
+        project_repo_name = github_repo_name.to_string();
+    } else {
+        project_repo_name = format!("{}/{}", &project_user_name, &circleci_repo_name);
+    }
+
+    println!("repo name {}", project_repo_name);
+
+
+    let api_endpoints = format!("https://api.github.com/repos/{}/pulls?head={}:{}&state=open", 
+        &project_repo_name,
+        &project_user_name, 
+        &project_branch_name);
+
+    api_endpoints
+}
--- cli/src/utils/mod.rs
+++ cli/src/utils/mod.rs
@@ -1,8 +1,10 @@
 pub mod website;
 pub mod issue;
 pub mod issue_info;
+pub mod github;
 
 pub(crate) use self::website::{Website};
 pub(crate) use self::issue::{Issue};
 pub(crate) use self::issue_info::{IssueInfo};
 
+pub(crate) use self::github::{get_api};
