27cfd03ca1e5c24223140f3ecfa7d59153065e88
j-mendez <jeff@a11ywatch.com>
2022-05-20 09:49:47 -0400

    chore(cli): revert to slow stream for network handling across paas services

--- cli/src/commands/api/rest.rs
+++ cli/src/commands/api/rest.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::env;
 use ureq::{Error, post, json};
+use std::time::Duration;
 use crate::utils::{Website};
 use crate::EXTERNAL;
 use crate::fs::temp::{TempFs};
@@ -47,26 +48,66 @@ impl ApiClient {
 
         Ok(resp)
     }
-    // Site wide scan [Stream]
+    // /// Site wide scan
+    // pub fn _walk_website(url: &str, file_manager: &TempFs) -> Result<CrawlApiResult, Error> {
+    //     let target_destination: String = match env::var(EXTERNAL) {
+    //         Ok(_) => "https://api.a11ywatch.com",
+    //         Err(_) => "http://127.0.0.1:3280",
+    //     }.to_string();
+
+    //     let request_destination = format!("{}/api/crawl", target_destination);
+    //     let token = file_manager.get_token();
+            
+    //     let resp: CrawlApiResult = if !token.is_empty() {
+    //         post(&request_destination)
+    //         .set("Authorization", &token)
+    //     } else {
+    //         post(&request_destination)
+    //     }.send_json(json!({
+    //         "websiteUrl": url
+    //     }))?.into_json()?;
+
+    //     Ok(resp)
+    // }
+
+    /// Site wide scan [Stream]
     pub fn crawl_website(url: &str, file_manager: &TempFs) -> Result<CrawlApiResult, Error> {
         let target_destination: String = match env::var(EXTERNAL) {
             Ok(_) => "https://api.a11ywatch.com",
             Err(_) => "http://127.0.0.1:3280",
         }.to_string();
 
-        let request_destination = format!("{}/api/crawl", target_destination);
+        let request_destination = format!("{}/api/crawl-stream", target_destination);
         let token = file_manager.get_token();
+        
+        let agent = ureq::builder()
+            .timeout(Duration::from_secs(500))
+            .build();
             
-        let resp: CrawlApiResult = if !token.is_empty() {
-            post(&request_destination)
+        let resp: Vec<CrawlApiResult> = if !token.is_empty() {
+            agent.post(&request_destination)
+            .set("Transfer-Encoding", "chunked")
             .set("Authorization", &token)
         } else {
-            post(&request_destination)
+            agent.post(&request_destination)
+            .set("Transfer-Encoding", "chunked")
         }.send_json(json!({
             "websiteUrl": url
         }))?.into_json()?;
 
-        Ok(resp)
+        let mut results: CrawlApiResult = CrawlApiResult::default();
+
+        // Get the first valid results. TODO: use real time stream endpoint for results.
+        for r in &resp {
+            let default_data: Vec<Website> = Vec::new();
+            let data = r.data.as_ref().unwrap_or(&default_data);
+
+            if !data.is_empty() {
+                results = r.to_owned();
+            };
+        };
+
+        Ok(results)
     }
 }
 
