d674d5e9f0047fa95013eec0eee0da413e571592
Jeff Mendez <jeff@a11ywatch.com>
2022-12-11 09:15:13 -0500

    chore(cli): add linux os type set crawler image (#32)
    * ci(test): add base image crawler test
    
    * chore(cli): add linux os variation crawler set

--- cli/src/fs/temp.rs
+++ cli/src/fs/temp.rs
@@ -216,10 +216,6 @@ impl TempFs {
         let env_path = format!("{}/.env", &self.app_dir);
         let env_path_tmp = format!("{}/env.txt", &self.app_dir);
 
-        // built from Apple M1 Max
-        let m1_max_chip = cfg!(all(target_os = "macos", target_arch = "aarch64", target_pointer_width = "64"));
-        let linux = cfg!(target_os = "linux");
-
         if !Path::new(&env_path).exists() {
             File::create(&env_path)?;
         };
@@ -253,22 +249,17 @@ impl TempFs {
         // map allowed env keys to file
         for line in reader.lines() {
             if let Ok(item) = line {
+                // crawler exist in env path ignore re-write
+                if item.starts_with(&"CRAWLER_IMAGE=") {
+                    wrote_crawler = true;
+                };
                 if !cv_token.is_empty() && item.contains(&c_v_s_k) {
                     writer.write_all(format!("{c_v_s_k}={}\n", cv_token).to_string().as_bytes())?;
                     wrote_c_v_s_k = true;
                 } else if !cv_url.is_empty() && item.contains(&c_v_e) {
                     writer.write_all(format!("{c_v_e}={}\n", cv_url).to_string().as_bytes())?;
                     wrote_c_v_e = true;
-                } else if m1_max_chip && item.contains(&"CRAWLER_IMAGE=darwin-arm64") {
-                    writer.write_all("CRAWLER_IMAGE=darwin-arm64\n".to_string().as_bytes())?;
-                    wrote_crawler = true;
-                } else if linux && item.contains(&"CRAWLER_IMAGE=debian") {
-                    writer.write_all("CRAWLER_IMAGE=debian\n".to_string().as_bytes())?;
-                    wrote_crawler = true;
-                } else if linux && item.contains(&"CRAWLER_IMAGE=alpine") {
-                    writer.write_all("CRAWLER_IMAGE=alpine\n".to_string().as_bytes())?;
-                    wrote_crawler = true;
-                } else {
+                }  else {
                     writer.write_all(format!("{}\n", item).to_string().as_bytes())?;
                 };
             }
@@ -277,6 +268,7 @@ impl TempFs {
         if !cv_token.is_empty() && !wrote_c_v_s_k {
             writer.write_all(format!("{c_v_s_k}={}\n", cv_token).to_string().as_bytes())?;
         };
+
         if !cv_url.is_empty() && !wrote_c_v_e {
             writer.write_all(
                 format!("COMPUTER_VISION_ENDPOINT={}\n", cv_url)
@@ -286,12 +278,24 @@ impl TempFs {
         };
 
         if !wrote_crawler {
-            if m1_max_chip {
+            // m1 max
+            if cfg!(all(target_os = "macos", target_arch = "aarch64", target_pointer_width = "64")) {
                 writer.write_all("CRAWLER_IMAGE=darwin-arm64\n".to_string().as_bytes())?;
-            } else if linux {
-                writer.write_all("CRAWLER_IMAGE=debian\n".to_string().as_bytes())?;
+            } else if cfg!(target_os = "linux") {
+                let info = os_info::get();
+                let os_type = info.os_type();
+
+                let c_os = if os_type == os_info::Type::Ubuntu {
+                    "ubuntu"
+                } else if os_type == os_info::Type::Alpine {
+                    "alpine"
+                } else {
+                    "debian"
+                };
+
+                writer.write_all(format!("CRAWLER_IMAGE={c_os}\n").to_string().as_bytes())?;
             };
-        }
+        };
 
         let mut file = OpenOptions::new()
             .write(true)
